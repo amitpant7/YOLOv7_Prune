d9fee904646f093e6db4163f8656b42a11c37dd5
Amit <amitrajpant7@gmail.com>
2024-07-15 13:28:59 +0545

    add pruner

--- pruner.py
+++ pruner.py
@@ -156,6 +156,9 @@ def train(hyp, opt, device, tb_writer=None):
         check_dataset(data_dict)  # check
     train_path = data_dict["train"]
     test_path = data_dict["val"]
+    
+    
+    ####################
 
     # Freeze
     freeze = [
